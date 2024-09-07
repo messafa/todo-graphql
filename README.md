d492c922ed5ab116704fc86ddee3ed23f907d12b
Messafa Mohammed <messafamohammed.mm@gmail.com>
2024-09-07 06:44:11 -0700

    Refactor Edit component to disable eslint for prop-types and import useState, gql, and useMutation

---
+++ README.md
@@ -0,0 +1,1 @@
+###testing Graphql with express
--- client/src/components/buttons/Edit.jsx
+++ client/src/components/buttons/Edit.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react';
 import { gql, useMutation } from '@apollo/client';
 import { Pencil } from "lucide-react";
